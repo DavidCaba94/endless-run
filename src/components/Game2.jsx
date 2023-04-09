a8c2f446f5f3b78a3b25c08b1c032c335a68859d
DavidCaba6194 <cabayosoyasi@gmail.com>
2023-04-09 21:09:05 +0200

    Mejoras de estilos y motor exclusivo para Flappy

--- src/components/Game2.jsx
+++ src/components/Game2.jsx
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
 import { GameEngine } from 'react-native-game-engine'
-import entities from '../entities'
+import entities from '../entities/flappybird'
 import Physics from '../utils/physics'
 import { Link } from 'react-router-native'
 
@@ -33,36 +33,68 @@ const Game2 = () => {
                 }
                 }}
                 style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
-            >
+                >
                 <StatusBar style="auto" hidden={true} />
 
             </GameEngine>
 
             {!running ?
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <TouchableOpacity style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
-                onPress={() => {
-                setCurrentPoints(0)
-                setRunning(true)
-                gameEngine.swap(entities())
-                }}>
-                <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
-                START GAME
-                </Text>
-            </TouchableOpacity>
-            <Link to={'/'} style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10, marginTop: 10 }}>
-                <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
-                HOME
-                </Text>
-            </Link>
-            <Link to={'/top'} style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10, marginTop: 10 }}>
-                <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
-                TOP
-                </Text>
-            </Link>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                    setCurrentPoints(0)
+                    setRunning(true)
+                    gameEngine.swap(entities())
+                    }}>
+                    <Text style={styles.textButton}>
+                    START GAME
+                    </Text>
+                </TouchableOpacity>
+                <Link to={'/'} style={styles.button}>
+                    <Text style={styles.textButton}>
+                    HOME
+                    </Text>
+                </Link>
+                <Link to={'/top'} style={styles.button}>
+                    <Text style={styles.textButton}>
+                    TOP
+                    </Text>
+                </Link>
             </View> : null}
         </View>
     )
 }
+
+const styles =  StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%'
+    },
+    button: {
+        width: 170,
+        backgroundColor: '#0378ff',
+        padding: 10,
+        borderRadius: 5,
+        borderWidth: 4,
+        borderColor: '#ffffff',
+        textAlign: 'center',
+        margin: 'auto',
+        marginTop: 10
+    },
+    textButton: {
+        width: '100%',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#ffffff',
+        textAlign: 'center'
+    },
+    image: {
+        position: 'absolute',
+        width: 50,
+        height: 50,
+        marginLeft: 200,
+        marginTop: 500
+    }
+})
 
 export default Game2
